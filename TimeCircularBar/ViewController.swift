078fdf3df88abf2acbfec5421887b94c168bfdfb
byLLIPyT <byllipyt@icloud.com>
2021-07-26 13:14:19 +0300

    + initial and work with UI

--- TimeCircularBar/ViewController.swift
+++ TimeCircularBar/ViewController.swift
@@ -9,11 +9,100 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some text"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let shapeView: UIImageView = {
+        let clockView = UIImageView()
+        clockView.image = UIImage(named: "circle")
+        clockView.translatesAutoresizingMaskIntoConstraints = false
+        return clockView
+    }()
+    
+    let cloclLabel: UILabel = {
+        let clockLabel = UILabel()
+        clockLabel.text = "10"
+        clockLabel.font = UIFont.boldSystemFont(ofSize: 60)
+        clockLabel.textColor = .black
+        clockLabel.textAlignment = .center
+        clockLabel.translatesAutoresizingMaskIntoConstraints = false
+        return clockLabel
+    }()
+    
+    let startButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 20
+        button.setTitle("START", for: .normal)
+        button.backgroundColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var timer = Timer()
+    var durationTimer = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        setConstraints()
+        startButton.addTarget(self, action: #selector(timerStart), for: .touchUpInside)
     }
 
+    @objc func timerStart() {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(changeTime), userInfo: nil, repeats: true)
+        print("go")
+    }
 
+    @objc func changeTime() {
+        
+        durationTimer -= 1
+        cloclLabel.text = "\(durationTimer)"
+        
+        if durationTimer == 0 {
+            timer.invalidate()
+        }
+    }
 }
 
+extension ViewController {
+    func setConstraints() {
+        view.addSubview(messageLabel)
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            messageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        view.addSubview(shapeView)
+        NSLayoutConstraint.activate([
+            shapeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            shapeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shapeView.heightAnchor.constraint(equalToConstant: 300),
+            shapeView.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        view.addSubview(cloclLabel)
+        NSLayoutConstraint.activate([
+            cloclLabel.centerXAnchor.constraint(equalTo: shapeView.centerXAnchor),
+            cloclLabel.centerYAnchor.constraint(equalTo: shapeView.centerYAnchor)
+        ])
+        
+        view.addSubview(startButton)
+        NSLayoutConstraint.activate([
+            startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.heightAnchor.constraint(equalToConstant: 70),
+            startButton.widthAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+}
